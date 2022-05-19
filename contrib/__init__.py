c71d9b82c3501aae96164ca73383cbdfd0b6809e
Hannes Hollmann <hannes.hollmann@anu.edu.au>
2022-05-19 17:41:38 +1000

    Added a second example

--- contrib/__init__.py
+++ contrib/__init__.py
@@ -104,76 +104,48 @@ class gravity(auxclass):
 
         elif self._ieg == 1:
             
-            name =__name__
-            
-            # within package/mymodule1.py, for example
-            
-            tmp = pkgutil.get_data(__name__, "data/gravmodel1.txt")
-            tmp2=tmp.decode("utf-8") 
-            self.m=np.loadtxt(StringIO(tmp2))
-            #tmp = pkgutil.get_data(__name__, "data/gravmodel1.txt")
-            #tmp2=np.frombuffer(tmp, dtype='S3')
-            #self.m=tmp2.astype(np.float)
-            del  tmp, tmp2
-            
-            #self.m=tmp3.astype(float)
-            #del tmp, tmp2, tmp3
-            
-            self.lmx = 12
-            self.lmy = 12
-            self.lmz = 12
-            self.lrx = 17
-            self.lry = 17
-            ##del tmp
-            
-            # Receiver locations in x and y direction
-            x_rec = np.linspace(-80.0, 80.0, self.lrx)
-            y_rec = np.linspace(-80.0, 80.0, self.lry)
-            tmp=cartesian((x_rec,y_rec))
+            # Define number of model cells and recording locations
+            self.lmx = 9
+            self.lmy = 1
+            self.lmz = 3
+            self.lrx = 9
+            self.lry = 1
+
+            ## Create node coordinates:
+            x_node_slice = np.linspace(-45, 45, self.lmx+1)
+            y_node_slice = np.linspace(-1000, 1000, self.lmy+1)
+            z_node_slice = np.linspace(-30, 0, self.lmz+1)
+            self.x_nodes, self.y_nodes, self.z_nodes = auxclass.node_maker(x_node_slice, y_node_slice, z_node_slice)
+
+            ## Define recording coordinates
+            x_rec = np.linspace(-40, 40, self.lrx)
+            y_rec = np.zeros(np.shape(x_rec))
+            x_rec=x_rec[:, np.newaxis]
+            y_rec=y_rec[:, np.newaxis]
+            tmp=np.append(x_rec,y_rec,axis=1) 
             z_rec=np.zeros(len(tmp))
             z_rec=z_rec[:, np.newaxis]
-            self.rec_coords=np.append(tmp,z_rec,axis=1)
-            del tmp
+            self.rec_coords=np.append(tmp,z_rec,axis=1) 
+            del tmp 
+            
+            print(x_rec)
+            print(self.rec_coords)
 
-            # Create array of all nodes for each direction
-            x_node_slice=np.linspace(-30,30,self.lmx+1)
-            y_node_slice=np.linspace(-30,30,self.lmy+1)
-            z_node_slice=np.linspace(0,-60,self.lmz+1)
-            z_node_slice=np.flipud(z_node_slice)
-            # Change boundary cells to create larger model:
-            x_node_slice[0]=x_node_slice[0]-995
-            y_node_slice[0]=y_node_slice[0]-995
-            x_node_slice[-1]=x_node_slice[-1]+995
-            y_node_slice[-1]=y_node_slice[-1]+995
 
-            # Combine the 3 node arrays to get start&finish of each prism edge
-            # 2 rows per array: Start and finish of each edge
-            coords_p1=cartesian((z_node_slice[0:-1],y_node_slice[0:-1],x_node_slice[0:-1]))
-            coords_p2=cartesian((z_node_slice[1:],y_node_slice[1:],x_node_slice[1:]))
-
-            # Bring output in order for x,y,z
-            temp1=coords_p1[:,0]
-            temp2=coords_p2[:,0]
-            temp1=temp1[:, np.newaxis]
-            temp2=temp2[:, np.newaxis]
-            self.z_nodes=np.append(temp1, temp2, axis=1)
-
-            temp1=coords_p1[:,1]
-            temp2=coords_p2[:,1]
-            temp1=temp1[:, np.newaxis]
-            temp2=temp2[:, np.newaxis]
-            self.y_nodes=np.append(temp1, temp2, axis=1)
-
-            temp1=coords_p1[:,2]
-            temp2=coords_p2[:,2]
-            temp1=temp1[:, np.newaxis]
-            temp2=temp2[:, np.newaxis]
-            self.x_nodes=np.append(temp1, temp2, axis=1)
-            del temp1, temp2
-                
+            ## Create model and insert density anomaly 
+            # Create model
+            m = np.zeros((self.lmx)*(self.lmy)*(self.lmz))
+            # Change specific cells to higher densities (anomaly)
+            anomaly=1000
+            m = auxclass.inject_density(m,self.x_nodes, self.y_nodes, self.z_nodes, -5,-1000,0, anomaly)
+            m = auxclass.inject_density(m,self.x_nodes, self.y_nodes, self.z_nodes, -15,-1000,-10, anomaly)
+            m = auxclass.inject_density(m,self.x_nodes, self.y_nodes, self.z_nodes, -5,-1000,-10, anomaly)
+            m = auxclass.inject_density(m,self.x_nodes, self.y_nodes, self.z_nodes, 5,-1000,-10, anomaly)
+            self.m = auxclass.inject_density(m,self.x_nodes, self.y_nodes, self.z_nodes, -5,-1000,-20, anomaly)
+            
         else:
+            
             print("Error - example number not defined")
-    
     
     def get_model(self):
         """
@@ -183,19 +155,21 @@ class gravity(auxclass):
         --------------------
         *args
 
-        data_x:
-        data_y:
-        data_z:
-        m:
-        rec_coords:
-        x_nodes:
-        y_nodes:
-        z_nodes:
-        lmx:
-        lmy:
-        lmz:
-        lrx:
-        lry:
+        data_x: ?
+        data_y: ?
+        data_z: ?
+        m: The model in a 1-D array containing densities
+        rec_coords: Array containing coonrdinates of recording stations [3xN]
+        x_nodes: X-coordinates of all nodes in model [2xN]
+        y_nodes: Y-coordinates of all nodes in model [2xN]
+        z_nodes: Z-coordinates of all nodes in model [2xN]
+        --------
+        For plotting purposes:
+        lmx: Number of cells in model; x-direction
+        lmy: Number of cells in model; y-direction
+        lmz: Number of cells in model; z-direction
+        lrx: Number of recording stations; x-direction
+        lry: Number of recording stations; x-direction
 
         --------------------
         """
@@ -250,7 +224,7 @@ class gravity(auxclass):
             g=-gxx-gyy
         return g
 
-    def _calculate_gravity(self, model, x_final, y_final, z_final, recvec, jacobian):
+    def _calculate_gravity(self, model, x_nodes, y_nodes, z_nodes, recvec, jacobian):
     
         from scipy.constants import G as G
         # Tolerance implementation follows Nagy et al., 2000
@@ -259,13 +233,13 @@ class gravity(auxclass):
         #gy_rec=np.zeros(len(recvec))
         gz_rec=np.zeros(len(recvec))
         if jacobian==True:
-            #Jx_rec=np.zeros([len(recvec),len(x_final)])
-            #Jy_rec=np.zeros([len(recvec),len(x_final)])
-            Jz_rec=np.zeros([len(recvec),len(x_final)])
+            #Jx_rec=np.zeros([len(recvec),len(x_nodes)])
+            #Jy_rec=np.zeros([len(recvec),len(x_nodes)])
+            Jz_rec=np.zeros([len(recvec),len(x_nodes)])
         for recno in range(len(recvec)):
-            dx=x_final-recvec[recno,0]
-            dy=y_final-recvec[recno,1]
-            dz=z_final-recvec[recno,2]
+            dx=x_nodes-recvec[recno,0]
+            dy=y_nodes-recvec[recno,1]
+            dz=z_nodes-recvec[recno,2]
             min_x=np.min(np.diff(dx))
             min_y=np.min(np.diff(dy))
             min_z=np.min(np.diff(dz))
@@ -313,9 +287,9 @@ class gravity(auxclass):
     def plot_model(self, m, data):
         if self._ieg == 0:
             
-            gx_rec=data.gx_rec
-            gy_rec=data.gy_rec
-            gz_rec=data.gz_rec
+            # gx_rec=data.gx_rec
+            # gy_rec=data.gy_rec
+            gz_rec=data
             
             lrx=self.lrx
             lry=self.lry
@@ -327,7 +301,7 @@ class gravity(auxclass):
             limy=max(self.rec_coords[:,1])+(self.rec_coords[1,1]-self.rec_coords[0,1])*0.5
             
             plt.figure(figsize=(17, 12))
-            plt.subplot(2, 2, 1)
+            plt.subplot(1, 2, 1)
             plt.scatter(self.rec_coords[:,1],self.rec_coords[:,0],s=0.3,color='k')
             plt.imshow(np.reshape(gz_rec,[lrx,lry]),extent=[-limy,limy,-limx,limx])
             plt.title('2D view of gz')
@@ -336,7 +310,7 @@ class gravity(auxclass):
             plt.colorbar(label="Gravity [mGal]")
 
             model2d=m.reshape(lmx,lmy,lmz)
-            plt.subplot(2, 2, 2)
+            plt.subplot(1, 2, 2)
             plt.title("Model slice at z = 30 m")
             plt.scatter(self.rec_coords[:,1],self.rec_coords[:,0],s=3,color='r')
             plt.imshow(model2d[7][:][:],extent=[-30,30,-30,30])
@@ -346,21 +320,51 @@ class gravity(auxclass):
             plt.xlim([-30,30])
             plt.ylim([-30,30])
 
-            plt.subplot(2, 2, 3)
-            plt.imshow(np.reshape(gx_rec,[lrx,lry]),extent=[-limy,limy,-limx,limx])
-            plt.title('2D view of gx')
-            plt.xlabel('y [m]')
-            plt.ylabel('x [m]')
-            plt.colorbar(label="Gravity [mGal]")
+#             plt.subplot(2, 2, 3)
+#             plt.imshow(np.reshape(gx_rec,[lrx,lry]),extent=[-limy,limy,-limx,limx])
+#             plt.title('2D view of gx')
+#             plt.xlabel('y [m]')
+#             plt.ylabel('x [m]')
+#             plt.colorbar(label="Gravity [mGal]")
 
-            plt.subplot(2, 2, 4)
-            plt.imshow(np.reshape(gy_rec,[lrx,lry]),extent=[-limy,limy,-limx,limx])
-            plt.title('2D view of gy')
-            plt.xlabel('y [m]')
-            plt.ylabel('x [m]')
-            plt.colorbar(label="Gravity [mGal]")
+#             plt.subplot(2, 2, 4)
+#             plt.imshow(np.reshape(gy_rec,[lrx,lry]),extent=[-limy,limy,-limx,limx])
+#             plt.title('2D view of gy')
+#             plt.xlabel('y [m]')
+#             plt.ylabel('x [m]')
+#             plt.colorbar(label="Gravity [mGal]")
             plt.show()
+        elif self._ieg == 1:
+            
+            gz_rec=data
+            
+            lrx=self.lrx
+            lry=self.lry
+            lmx=self.lmx
+            lmy=self.lmy
+            lmz=self.lmx
 
+            limx=max(self.rec_coords[:,0])+(self.rec_coords[1,0]-self.rec_coords[0,0])*0.5
+            limy=max(self.rec_coords[:,1])+(self.rec_coords[1,1]-self.rec_coords[0,1])*0.5
+            
+            plt.figure(figsize=(17, 8))
+            plt.subplot(1, 2, 1)
+            plt.scatter(self.rec_coords[:,0],self.rec_coords[:,1],s=0.3,color='k')
+            plt.imshow(np.reshape(m,[self.lmz,self.lmx]))
+            plt.title('2D view of the model')
+            plt.xlabel('y [m]')
+            plt.ylabel('z [m]')
+            plt.colorbar(label="Density [kg/m^3]")
+
+            plt.subplot(1, 2, 2)
+            plt.plot(self.rec_coords[:,0], data) 
+            plt.title('gz')
+            plt.xlabel('Distance [m]')
+            plt.ylabel('Gravity [mGal]')
+            plt.grid()
+
+            plt.show()
+            
         else:
             print("Error - example number not defined")
     
