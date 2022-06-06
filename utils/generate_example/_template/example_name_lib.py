10a78a51c4c4ae890f4caaf5daf6304620d1464d
Jiawen He <hanghur@gmail.com>
2022-06-06 16:34:17 +1000

    refactor: improved template

--- utils/generate_example/_template/example_name_lib.py
+++ utils/generate_example/_template/example_name_lib.py
@@ -2,6 +2,8 @@
 
 Please put all the library code that will be needed in the notebook in this file.
 
+This file is optional. You can safely delete it if not needed.
+
 The file name should end with "_lib.py", otherwise our bot may fail when generating
 scripts for Sphinx Gallery. Furthermore, we recommend the file name to start with your
 forward problem name, to align well with the naming of Jupyter notebook.
