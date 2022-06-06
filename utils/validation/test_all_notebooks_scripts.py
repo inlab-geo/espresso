a873b85b3c386cae8f31f05965c3b1015745cd59
Jiawen He <hanghur@gmail.com>
2022-06-06 16:35:17 +1000

    feat: progress in testing all scripts

--- utils/validation/test_all_notebooks_scripts.py
+++ utils/validation/test_all_notebooks_scripts.py
@@ -4,8 +4,12 @@
 
 import subprocess
 from glob import glob
+import os
 
 NOTEBOOKS_FOLDER = "notebooks"
+OUTPUT_FOLDER = "utils/validation/_output"
+VALIDATION_FOLDER = "utils/validation/_validation_output"
+
 PYTHON = "python"
 
 class bcolors:
@@ -28,12 +32,46 @@ def listpy_nohidden_notlib(dir):
 
 
 def main():
-    # collect all scripts under notebooks/ folder
-    all_scripts = glob(f"{NOTEBOOKS_FOLDER}/*/*.py")
-    all_scripts = [name for name in all_scripts if "lib.py" not in name]
+    # # collect all scripts under notebooks/ folder
+    # all_scripts = glob(f"{NOTEBOOKS_FOLDER}/*/*.py")
+    # all_scripts = [name for name in all_scripts if "lib.py" not in name]
     # run them
-    for script in all_scripts:
-        subprocess.call([PYTHON, script])
+    # for script in all_scripts:
+    #     subprocess.call([PYTHON, script])
+    print("Validation starts.")
+    subprocess.run(["rm", "-rf", OUTPUT_FOLDER])
+    os.mkdir(OUTPUT_FOLDER)
+
+    for example_dir in listdir_nohidden():
+        print(f"\nTesting example - {example_dir} ...")
+        out_dir = example_dir.replace(NOTEBOOKS_FOLDER, OUTPUT_FOLDER)
+        os.mkdir(out_dir)
+        val_dir = example_dir.replace(NOTEBOOKS_FOLDER, VALIDATION_FOLDER)
+        for script in listpy_nohidden_notlib(example_dir):
+            script_name = script.split("/")[-1]
+            out_subdir = f"{out_dir}{script_name[:-3]}"
+            os.mkdir(out_subdir)
+            val_subdir = f"{val_dir}{script_name[:-3]}"
+            print(f"script: {script}")
+            print(f"\tsaving outputs to folder {out_subdir}")
+            res = subprocess.run([
+                PYTHON, 
+                script, 
+                "--no-show-plot", 
+                "--save-plot", 
+                "--show-summary",
+                "--output-dir",
+                out_subdir,
+            ], stdout=subprocess.PIPE, text=True)
+            with open(f"{out_subdir}/log.txt", "w") as log_file:
+                log_file.write(res.stdout)
+            print(f"\tcomparing outputs with folder {val_subdir}")
+        try:   # remove empty folders
+            os.rmdir(out_dir)
+        except:
+            pass
+
+    print("\nOK.")
 
 
 if __name__ == "__main__":
