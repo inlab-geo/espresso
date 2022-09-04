6fbd3f629fbd8be013b0d810b4ed43b2fcf06790
Andrew Valentine <andrew.valentine@durham.ac.uk>
2022-09-04 19:18:55 +0100

    Modified to give generic error message

--- src/cofi_espresso/exceptions.py
+++ src/cofi_espresso/exceptions.py
@@ -8,4 +8,16 @@ class EspressoError(Exception):
 # ... except ValueError
 class InvalidExampleError(EspressoError, ValueError):
     """ Raised if user attempts to instantiate an example number that does not exist """
-    pass
+    def __init__(self, *args):
+        super().__init__(*args)
+    def __str__(self):
+        super_msg = super().__str__()
+        msg = "Unrecognised example number.\n\nPlease refer to the Espresso documentation " \
+              "(https://cofi-espresso.readthedocs.io/)\nfor full details of the examples " \
+              "provided within this test problem."
+        if len(super_msg)>0:
+            return msg+"\n\n"+super_msg
+        else:
+            return msg
+
+
