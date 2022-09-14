3eb1695c889fb46b1f23b0268ff11839a28a788a
Jiawen He <hanghur@gmail.com>
2022-09-14 13:26:07 +1000

    feat: starting on new example FMMTOMO

---
+++ contrib/fmm_tomography/__init__.py
@@ -0,0 +1,15 @@
+"""Module initialisation file
+
+It's NOT recommended to put extra code inside this file. Code inside this file will be
+executed when this submodule is imported, so adding things in this file can slow down
+the importing process of `cofi_espresso`.
+
+For contributors, add any intialisation code for your problem into fmm_tomography.py, 
+under the method `__init__()` of the class `FmmTomography`.
+
+Don't touch this file unless you know what you are doing :)
+"""
+
+from .fmm_tomography import *
+
+__all__ = [ "FmmTomography" ]
