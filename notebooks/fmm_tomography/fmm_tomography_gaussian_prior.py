6c1d1b240642a58c47936762a9b0e45fdcf6830e
Jiawen He <hanghur@gmail.com>
2022-10-07 13:14:57 +1100

    feat: progress in fmm; just needs cleaning up

--- notebooks/fmm_tomography/fmm_tomography_gaussian_prior.py
+++ notebooks/fmm_tomography/fmm_tomography_gaussian_prior.py
@@ -7,19 +7,16 @@ Last updated July 2022
 """
 
 import numpy as np
-import findiff
 
 from cofi import BaseProblem, InversionOptions, Inversion
-# from cofi.utils import QuadraticReg
 from cofi_espresso import FmmTomography
 
 # get espresso problem FmmTomography information
 fmm = FmmTomography()
 model_size = fmm.model_size
-model_shape = (32, 48)      # TODO to be replaced by `fmm.model_shape`
+model_shape = fmm.model_shape
 data_size = fmm.data_size
-ref_start_model = fmm.starting_model
-ref_start_slowness = 1/ref_start_model
+ref_start_slowness = fmm.starting_model
 
 # define model covariance matrix
 corrx=3.0
@@ -50,17 +47,17 @@ np.fill_diagonal(Cdi, 1/sigma**2)
 
 # define chi square function
 def chi_square(model_slowness, esp_fmm, Cd_inv, Cp_inv):
-    pred = esp_fmm.forward(1/model_slowness)
+    pred = esp_fmm.forward(model_slowness)
     residual = esp_fmm.data - pred
-    model_diff = model_slowness - 1/esp_fmm.starting_model
+    model_diff = model_slowness - ref_start_slowness
     return residual.T @ Cd_inv @ residual + model_diff.T @ Cp_inv @ model_diff
 def gradient(model_slowness, esp_fmm, Cd_inv, Cp_inv):
-    pred, jac = esp_fmm.forward(1/model_slowness, with_jacobian=True)
+    pred, jac = esp_fmm.forward(model_slowness, with_jacobian=True)
     residual = esp_fmm.data - pred
-    model_diff = model_slowness - 1/esp_fmm.starting_model
+    model_diff = model_slowness - ref_start_slowness
     return -jac.T @ Cd_inv @ residual + Cp_inv @ model_diff
 def hessian(model_slowness, esp_fmm, Cd_inv, Cp_inv):
-    A = esp_fmm.jacobian(1/model_slowness)
+    A = esp_fmm.jacobian(model_slowness)
     return A.T @ Cd_inv @ A + Cp_inv
 
 # define CoFI BaseProblem
@@ -79,8 +76,8 @@ inv_options.set_params(method=method, options={"xtol":1e-12})
 # define CoFI Inversion and run
 inv = Inversion(fmm_problem, inv_options)
 inv_result = inv.run()
-fig1 = fmm.plot_model(1/inv_result.model)
-fig1.savefig(f"fmm_gaussian_prior_scipy_{method}")
+fig1 = fmm.plot_model(inv_result.model)
+fig1.savefig(f"figs/fmm_gaussian_prior_scipy_{method}")
 
 # alternative approach
 def run_naive_newton():
@@ -93,7 +90,7 @@ def run_naive_newton():
         step = -np.linalg.inv(hess).dot(grad)
         m += step
     fig2 = fmm.plot_model(1/m)
-    fig2.savefig(f"fmm_gaussian_prior_naive_newton")
+    fig2.savefig(f"figs/fmm_gaussian_prior_naive_newton")
 
 # run_naive_newton()
 
--- notebooks/fmm_tomography/fmm_tomography_solver_script.py
+++ notebooks/fmm_tomography/fmm_tomography_solver_script.py
@@ -7,19 +7,18 @@ Last updated July 2022
 """
 
 import numpy as np
-import findiff
 
 from cofi import BaseProblem, InversionOptions, Inversion
-# from cofi.utils import QuadraticReg
+from cofi.utils import QuadraticReg
 from cofi_espresso import FmmTomography
+
 
 # get espresso problem FmmTomography information
 fmm = FmmTomography()
-model_size = fmm.model_size
-model_shape = (32, 48)      # TODO to be replaced by `fmm.model_shape`
-data_size = fmm.data_size
-ref_start_model = fmm.starting_model
-ref_start_slowness = 1/ref_start_model
+model_size = fmm.model_size         # number of model parameters
+model_shape = fmm.model_shape       # 2D spatial grids
+data_size = fmm.data_size           # number of data points
+ref_start_slowness = fmm.starting_model
 
 # define CoFI BaseProblem
 fmm_problem = BaseProblem()
@@ -27,67 +26,31 @@ fmm_problem.set_initial_model(ref_start_slowness)
 
 # add regularization: damping + smoothing
 damping_factor = 100
-flattening_factor = 1e4
-smoothing_factor = 0
-
-d2_dx2 = findiff.FinDiff(0, 1, 2)
-d2_dy2 = findiff.FinDiff(1, 1, 2)
-matx2 = d2_dx2.matrix(model_shape)
-maty2 = d2_dy2.matrix(model_shape)
-Dm2 = np.vstack((matx2.toarray(), maty2.toarray()))
-d_dx2 = findiff.FinDiff(0, 1, 1)
-d_dy2 = findiff.FinDiff(1, 1, 1)
-matx = d_dx2.matrix(model_shape)
-maty = d_dy2.matrix(model_shape)
-Dm1 = np.vstack((matx.toarray(), maty.toarray()))
-
-def damping_reg(slowness):
-    slowness_diff = slowness - ref_start_slowness
-    return damping_factor * slowness_diff.T @ slowness_diff
-def damping_grad(slowness):
-    return damping_factor * (slowness - ref_start_slowness)
-def damping_hess():
-    return damping_factor * np.eye((model_size))
-def flattening_reg(slowness):
-    weighted_slowness = Dm2 @ slowness
-    return flattening_factor * weighted_slowness.T @ weighted_slowness
-def flattening_grad(slowness):
-    return flattening_factor * Dm2.T @ Dm2 @ slowness
-def flattening_hess():
-    return flattening_factor * Dm2.T @ Dm2
-def smoothing_reg(slowness):
-    weighted_slowness = Dm2 @ slowness
-    return smoothing_factor * weighted_slowness.T @ weighted_slowness
-def smoothing_grad(slowness):
-    return smoothing_factor * Dm2.T @ Dm2 @ slowness
-def smoothing_hess():
-    return smoothing_factor * Dm2.T @ Dm2
-
-def reg(slowness):
-    return damping_reg(slowness) + flattening_reg(slowness) + smoothing_reg(slowness)
-def reg_grad(slowness):
-    return damping_grad(slowness) + flattening_grad(slowness) + smoothing_grad(slowness)
-reg_hess = damping_hess() + flattening_hess() + smoothing_hess()
-
+flattening_factor = 0
+smoothing_factor = 5e3
+reg_damping = QuadraticReg(damping_factor, model_size, "damping", ref_start_slowness)
+reg_flattening = QuadraticReg(flattening_factor, model_shape, "flattening")
+reg_smoothing = QuadraticReg(smoothing_factor, model_shape, "smoothing")
+reg = reg_damping + reg_flattening + reg_smoothing
 fmm_problem.set_regularization(reg)
-
-# TODO above to be replaced by cofi.utils (below)
-# reg_damping = QuadraticReg(damping_factor, model_size, "damping")
-# reg_smoothing = QuadraticReg(smoothing_factor, model_shape, "smoothing")
-# reg = reg_damping + reg_smoothing
-# fmm_problem.set_regularization(reg)
 
 sigma =  0.00001                   # Noise is 1.0E-4 is ~5% of standard deviation of initial travel time residuals
 def objective_func(slowness):
-    ttimes = fmm.forward(1/slowness)
+    ttimes = fmm.forward(slowness)
     residual = fmm.data - ttimes
-    return residual.T @ residual / sigma**2 + reg(slowness)
+    data_misfit = residual.T @ residual / sigma**2
+    model_reg = reg(slowness)
+    return  data_misfit + model_reg
 def gradient(slowness):
-    ttimes, A = fmm.forward(1/slowness, with_jacobian=True)
-    return -A.T @ (fmm.data - ttimes) / sigma**2 + reg_grad(slowness)
+    ttimes, A = fmm.forward(slowness, with_jacobian=True)
+    data_misfit_grad = -A.T @ (fmm.data - ttimes) / sigma**2
+    model_reg_grad = reg.gradient(slowness)
+    return  data_misfit_grad + model_reg_grad
 def hessian(slowness):
-    A = fmm.jacobian(1/slowness)
-    return A.T @ A / sigma**2 + reg_hess
+    A = fmm.jacobian(slowness)
+    data_misfit_hess = A.T @ A / sigma**2 
+    model_reg_hess = reg.hessian(slowness)
+    return data_misfit_hess + model_reg_hess
 
 fmm_problem.set_objective(objective_func)
 fmm_problem.set_gradient(gradient)
@@ -97,26 +60,26 @@ fmm_problem.set_hessian(hessian)
 inv_options = InversionOptions()
 inv_options.set_tool("scipy.optimize.minimize")
 method = "Newton-CG"
-inv_options.set_params(method=method)
+inv_options.set_params(method=method, options={"xtol":1e-10, "disp":True})
 
 # Define CoFI Inversion and run
 # inv = Inversion(fmm_problem, inv_options)
 # inv_result = inv.run()
-# fig1 = fmm.plot_model(1/inv_result.model)
-# fig1.savefig(f"fmm_{int(damping_factor)}_{int(smoothing_factor)}_scipy_{method}")
+# fig1 = fmm.plot_model(inv_result.model)
+# fig1.savefig(f"figs/fmm_{int(damping_factor)}_{int(smoothing_factor)}_scipy_{method}")
 
 # Run with another approach - iterative Newton full-step updates
-num_iterations = 3
+num_iterations = 5
 m = fmm_problem.initial_model
 for i in range(num_iterations):
     print(fmm_problem.objective(m))
     grad = fmm_problem.gradient(m)
     hess = fmm_problem.hessian(m)
     step = -np.linalg.inv(hess).dot(grad)
-    m += step
-fig2 = fmm.plot_model(1/m)
-fig2.savefig(f"fmm_{int(damping_factor)}_{int(flattening_factor)}_{int(smoothing_factor)}_naive_newton")
+    m += 0.5*step
+fig2 = fmm.plot_model(m)
+fig2.savefig(f"figs/fmm_{int(damping_factor)}_{int(flattening_factor)}_{int(smoothing_factor)}_simple_newton")
 
-# Plot the true model
-fig3 = fmm.plot_model(fmm.good_model)
-fig3.savefig("fmm_true_model")
+# # Plot the true model
+# fig3 = fmm.plot_model(fmm.good_model)
+# fig3.savefig("figs/fmm_true_model")
