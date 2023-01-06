bd3463c577c494f5b707682c66fdf9b4ca0a3bd2
Jiawen He <hanghur@gmail.com>
2023-01-06 12:17:18 +1100

    fix: ci gfortran version and fetch tags

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ try:
 except ImportError:
     print(
         "Please update pip, you need pip 10 or greater,\n"
-        " or you need to install the PEP 518 requirements in pyproject.toml yourself",
+        " or you need to instal/home/jiawen/espresso/.github/workflowsl the PEP 518 requirements in pyproject.toml yourself",
         file=sys.stderr,
     )
     raise
