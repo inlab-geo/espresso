b9855d8768b78006672ade6b7a1d3ffd994f99b9
jwhhh <jwhhh@users.noreply.github.com>
2023-04-01 01:31:53 +0000

    Update esp_build to output generated at 8ef941a

--- src/espresso/list_problems.py
+++ src/espresso/list_problems.py
@@ -79,10 +79,10 @@ _capability_matrix = {
         "misfit": 0,
         "log_likelihood": 0,
         "log_prior": 0,
-        "tmp_paths": 1,
-        "exe_fm2dss": 1,
         "tmp_files": 1,
-        "clean_tmp_files": 1
+        "tmp_paths": 1,
+        "clean_tmp_files": 1,
+        "exe_fm2dss": 1
     },
     "PumpingTest": {
         "model_size": 1,
@@ -185,8 +185,8 @@ _capability_matrix = {
         "misfit": 1,
         "log_likelihood": 0,
         "log_prior": 0,
-        "set_start_model": 1,
         "set_obs_data": 1,
-        "set_start_mesh": 1
+        "set_start_mesh": 1,
+        "set_start_model": 1
     }
 }
