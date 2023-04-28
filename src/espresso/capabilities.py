752fadf059b4f5875399be60646f5308ee251db3
jwhhh <hanghur@gmail.com>
2023-04-28 00:10:21 +0000

    Update esp_build to output generated at 4a111a0

--- src/espresso/capabilities.py
+++ src/espresso/capabilities.py
@@ -221,9 +221,9 @@ _capability_matrix = {
         "misfit": 1,
         "log_likelihood": 0,
         "log_prior": 0,
-        "set_start_mesh": 1,
+        "set_start_model": 1,
         "set_obs_data": 1,
-        "set_start_model": 1
+        "set_start_mesh": 1
     },
     "SimpleRegression": {
         "model_size": 1,
@@ -276,9 +276,9 @@ _capability_matrix = {
         "misfit": 0,
         "log_likelihood": 0,
         "log_prior": 0,
-        "clean_tmp_files": 1,
-        "exe_fm2dss": 1,
         "tmp_paths": 1,
-        "tmp_files": 1
+        "clean_tmp_files": 1,
+        "tmp_files": 1,
+        "exe_fm2dss": 1
     }
 }
