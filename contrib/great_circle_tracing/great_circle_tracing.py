698cfb1d9d878f4ce2236c7fcfba4d7cd573a0e1
Fabrizio Magrini <98057074+fmagrini@users.noreply.github.com>
2023-05-25 15:09:26 +1000

    Surface-wave velocity class

---
+++ contrib/great_circle_tracing/great_circle_tracing.py
@@ -0,0 +1,144 @@
+from espresso import EspressoProblem
+from espresso.exceptions import InvalidExampleError
+from espresso.utils import absolute_path
+from seislib.plotting import plot_map
+from seislib.utils import load_pickle
+import seislib.colormaps as scm
+import numpy as np
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+
+
+
+
+class GreatCircleTracing(EspressoProblem):
+    """Forward simulation class"""
+
+    metadata = {
+        "problem_title": "Surface-wave imaging",  # To be used in docs
+        "problem_short_description": "Surface-wave imaging",  # 1-3 sentences
+        # List of names e.g. author_names = ["Sally Smith", "Mark Brown"]
+        "author_names": [
+            "Fabrizio Magrini",
+        ],
+        # Contact for contributor/maintainer of espresso example
+        "contact_name": "Fabrizio Magrini",
+        "contact_email": "fabrizio.magrini@anu.edu.au",
+        # Reference to publication(s) that describe this example. In most
+        # cases there will only be a single entry in this list.
+        # List of (citation, doi) pairs e.g.
+        # citations = [("Newton, I (1687). Philosophiae naturalis principia mathematica.", "")]
+        # If there are no citations, use empty list `[]`
+        "citations": [
+            ("Surface-wave tomography using SeisLib: a Python package for \
+             multiscale seismic imaging, GJI,  vol 84, 1011-1030, 2022. \
+                 F. Magrini, L. Sebastian, E. K{\"a}stle, L. Boschi",
+            "https://doi.org/10.1093/gji/ggac236"
+            )
+                                                ],
+        # List of (title, address) pairs for any websites that
+        # should be linked in the documentation, e.g.
+        # linked_sites = [("Parent project on Github","https://github.com/user/repo"),
+        #                 ("Data source","https://www.data.com") ]
+        # If there are no links, use empty list `[]`
+        "linked_sites": [("PyPI Installation", "https://pypi.org/project/seislib/"), 
+                          ("SeisLib docs", "https://seislib.readthedocs.io/en/latest/")],
+    }
+    
+        
+    def __init__(self, example_number=1):
+        super().__init__(example_number)
+
+        if example_number == 1:
+            self._description = "Rayleigh-wave phase velocity across USA at 10 s period"
+            self.example_dict = load_pickle(absolute_path('./data/example1.pickle'))
+        else:
+            raise InvalidExampleError
+        self._data = self.example_dict['slowness']
+        self._jacobian = self.example_dict['jacobian']
+        self._good_model = self.example_dict['model']
+        ref_slowness = np.mean(self._data)
+        self._null_model = np.full(self._good_model.size, ref_slowness)
+        self.parameterization = self.example_dict['grid']
+
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def model_size(self):
+        return self._null_model.size
+
+    @property
+    def data_size(self):
+        return self._data.size
+
+    @property
+    def good_model(self):
+        return self._good_model
+
+    @property
+    def starting_model(self):
+        return self._null_model
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def covariance_matrix(self):  # optional
+        raise NotImplementedError
+
+    @property
+    def inverse_covariance_matrix(self):
+        raise NotImplementedError  # optional
+
+    def forward(self, model, with_jacobian=False):
+        jacobian = self.jacobian(model)
+        dpred = jacobian @ model
+        if with_jacobian:
+            return dpred, jacobian
+        return dpred 
+
+    def jacobian(self, model):
+        return self._jacobian
+
+    def plot_model(self, model, **kwargs):
+        velocity = 1 / model
+        vmean = np.mean(velocity)
+        fig = plt.figure()
+        ax = plt.subplot(111, projection=ccrs.Mercator())
+        ax.coastlines()
+        img, cb = plot_map(self.parameterization.mesh,
+                           velocity,
+                           ax=ax,
+                           cmap=scm.roma,
+                           vmin=vmean - vmean*0.1,
+                           vmax=vmean + vmean*0.1,
+                           show=False,
+                           **kwargs)       
+        cb.set_label('Phase velocity [m/s]')      
+        plt.tight_layout()
+        return fig
+
+    def plot_data(self, data, data2=None):
+        raise NotImplementedError  # optional
+
+    def misfit(self, data, data2):
+        return np.sum(np.square(data - data2))
+
+    def log_likelihood(self, data, data2):
+        raise NotImplementedError  # optional
+
+    def log_prior(self, model):
+        raise NotImplementedError  # optional
+
+
+
+
+
+
+
+
+
