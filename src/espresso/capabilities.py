ba9071dea83af734cdc458ecc827d5da2d5e6d77
jwhhh <jwhhh@users.noreply.github.com>
2023-08-09 04:07:22 +0000

    Update esp_build to output generated at cee9dd1

--- src/espresso/capabilities.py
+++ src/espresso/capabilities.py
@@ -181,10 +181,10 @@ _capability_matrix = {
         "misfit": 1,
         "log_likelihood": 0,
         "log_prior": 0,
-        "set_obs_data": 1,
-        "set_start_mesh": 1,
+        "list_capabilities": 1,
         "set_start_model": 1,
-        "list_capabilities": 1
+        "set_start_mesh": 1,
+        "set_obs_data": 1
     },
     "FmmTomography": {
         "model_size": 1,
@@ -202,9 +202,9 @@ _capability_matrix = {
         "misfit": 0,
         "log_likelihood": 0,
         "log_prior": 0,
-        "call_wavefront_tracker": 1,
         "list_capabilities": 1,
-        "tmp_files": 1
+        "tmp_files": 1,
+        "call_wavefront_tracker": 1
     },
     "SurfaceWaveTomography": {
         "model_size": 1,
@@ -222,9 +222,9 @@ _capability_matrix = {
         "misfit": 1,
         "log_likelihood": 0,
         "log_prior": 0,
-        "parameterization": 1,
         "list_capabilities": 1,
-        "example_dict": 1
+        "example_dict": 1,
+        "parameterization": 1
     },
     "ReceiverFunctionInversion": {
         "model_size": 1,
