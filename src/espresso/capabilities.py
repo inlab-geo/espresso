f56f61f053155a9e1051bfd97c695773362d4a29
jwhhh <hanghur@gmail.com>
2024-03-18 08:03:42 +0000

    Update esp_build to output generated at 71ff6c1

--- src/espresso/capabilities.py
+++ src/espresso/capabilities.py
@@ -184,8 +184,8 @@ _capability_matrix = {
         "misfit": 1,
         "log_likelihood": 1,
         "log_prior": 1,
-        "list_capabilities": 1,
-        "rf": 1
+        "rf": 1,
+        "list_capabilities": 1
     },
     "PumpingTest": {
         "model_size": 1,
@@ -221,9 +221,9 @@ _capability_matrix = {
         "misfit": 1,
         "log_likelihood": 0,
         "log_prior": 0,
-        "list_capabilities": 1,
         "example_dict": 1,
-        "parameterization": 1
+        "parameterization": 1,
+        "list_capabilities": 1
     },
     "FmmTomography": {
         "model_size": 1,
@@ -241,8 +241,8 @@ _capability_matrix = {
         "misfit": 0,
         "log_likelihood": 0,
         "log_prior": 0,
-        "list_capabilities": 1,
         "tmp_files": 1,
+        "list_capabilities": 1,
         "call_wavefront_tracker": 1
     },
     "SlugTest": {
@@ -279,9 +279,9 @@ _capability_matrix = {
         "misfit": 1,
         "log_likelihood": 0,
         "log_prior": 0,
+        "set_obs_data": 1,
         "set_start_model": 1,
         "list_capabilities": 1,
-        "set_obs_data": 1,
         "set_start_mesh": 1
     },
     "XrayTomography": {
@@ -318,8 +318,8 @@ _capability_matrix = {
         "misfit": 0,
         "log_likelihood": 0,
         "log_prior": 0,
-        "list_capabilities": 1,
-        "rf": 1
+        "rf": 1,
+        "list_capabilities": 1
     },
     "SimpleRegression": {
         "model_size": 1,
