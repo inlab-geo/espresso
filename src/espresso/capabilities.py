76217189db6344e4cd67a3da16faffcaf2c6ddeb
jwhhh <hanghur@gmail.com>
2024-05-24 06:26:15 +0000

    Update esp_build to output generated at 7e3d51f

--- src/espresso/capabilities.py
+++ src/espresso/capabilities.py
@@ -276,9 +276,9 @@ _capability_matrix = {
         "misfit": 0,
         "log_likelihood": 0,
         "log_prior": 0,
+        "call_wavefront_tracker": 1,
         "tmp_files": 1,
-        "list_capabilities": 1,
-        "call_wavefront_tracker": 1
+        "list_capabilities": 1
     },
     "PumpingTest": {
         "model_size": 1,
@@ -314,10 +314,10 @@ _capability_matrix = {
         "misfit": 1,
         "log_likelihood": 0,
         "log_prior": 0,
+        "set_start_mesh": 1,
         "set_start_model": 1,
-        "list_capabilities": 1,
         "set_obs_data": 1,
-        "set_start_mesh": 1
+        "list_capabilities": 1
     },
     "SurfaceWaveTomography": {
         "model_size": 1,
@@ -336,7 +336,7 @@ _capability_matrix = {
         "log_likelihood": 0,
         "log_prior": 0,
         "parameterization": 1,
-        "list_capabilities": 1,
-        "example_dict": 1
+        "example_dict": 1,
+        "list_capabilities": 1
     }
 }
