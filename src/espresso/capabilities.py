b82b16867e3b6eb12ad9d2f7381928366a6aca9a
fmagrini <98057074+fmagrini@users.noreply.github.com>
2024-06-11 07:51:42 +0000

    Update esp_build to output generated at fc35a2b

--- src/espresso/capabilities.py
+++ src/espresso/capabilities.py
@@ -166,8 +166,8 @@ _capability_matrix = {
         "misfit": 1,
         "log_likelihood": 1,
         "log_prior": 1,
-        "rf": 1,
-        "list_capabilities": 1
+        "list_capabilities": 1,
+        "rf": 1
     },
     "ReceiverFunctionInversionKnt": {
         "model_size": 1,
@@ -185,8 +185,8 @@ _capability_matrix = {
         "misfit": 0,
         "log_likelihood": 0,
         "log_prior": 0,
-        "rf": 1,
-        "list_capabilities": 1
+        "list_capabilities": 1,
+        "rf": 1
     },
     "GravityInversion": {
         "model_size": 1,
@@ -276,9 +276,9 @@ _capability_matrix = {
         "misfit": 0,
         "log_likelihood": 0,
         "log_prior": 0,
-        "call_wavefront_tracker": 1,
+        "list_capabilities": 1,
         "tmp_files": 1,
-        "list_capabilities": 1
+        "call_wavefront_tracker": 1
     },
     "PumpingTest": {
         "model_size": 1,
@@ -314,10 +314,10 @@ _capability_matrix = {
         "misfit": 1,
         "log_likelihood": 0,
         "log_prior": 0,
-        "set_start_model": 1,
-        "set_obs_data": 1,
+        "list_capabilities": 1,
         "set_start_mesh": 1,
-        "list_capabilities": 1
+        "set_start_model": 1,
+        "set_obs_data": 1
     },
     "SurfaceWaveTomography": {
         "model_size": 1,
@@ -335,8 +335,8 @@ _capability_matrix = {
         "misfit": 1,
         "log_likelihood": 0,
         "log_prior": 0,
+        "list_capabilities": 1,
         "example_dict": 1,
-        "parameterization": 1,
-        "list_capabilities": 1
+        "parameterization": 1
     }
 }
