59ae017da5f3d042b13e1e11d52d77092916b914
Nancy <hanghur@gmail.com>
2024-06-14 17:10:55 +1000

    feat: template files for structure overhaul

---
+++ espresso_machine/new_contribution/_template_files/README.md
@@ -0,0 +1,10 @@
+# Example Name Title
+
+
+<!-- Welcome to your new Espresso example! -->
+
+<!-- Following along with the 
+[contributor guide](https://geo-espresso.readthedocs.io/en/latest/contributor_guide/new_contrib.html#add-your-own-espresso-problem) 
+to get started. -->
+
+<!-- Please write anything you'd like to explain about the forward problem here -->
---
+++ espresso_machine/new_contribution/_template_files/__init__.py
@@ -0,0 +1,17 @@
+"""Module initialisation file
+
+It's NOT recommended to put extra code inside this file. Code inside this file will be
+executed when this submodule is imported, so adding things in this file can slow down
+the importing process of `espresso`.
+
+For contributors, add any intialisation code for your problem into example_name.py, 
+under the method `__init__()` of the class `ExampleName`.
+
+Try not to touch this file unless you are updating a version.
+"""
+
+from .example_name import *
+
+__all__ = ["ExampleName"]
+
+__version__ = "0.1.0"
