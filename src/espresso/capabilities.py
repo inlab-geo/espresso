e88f19dc823e6a1e4ce1fd8581e81165152e48aa
auggiemarignier <42379892+auggiemarignier@users.noreply.github.com>
2024-11-10 21:54:31 +0000

    Update esp_build to output generated at f00c8c1

--- src/espresso/capabilities.py
+++ src/espresso/capabilities.py
@@ -167,8 +167,8 @@ _capability_matrix = {
         "log_likelihood": 0,
         "log_prior": 0,
         "tmp_files": 1,
-        "call_wavefront_tracker": 1,
-        "list_capabilities": 1
+        "list_capabilities": 1,
+        "call_wavefront_tracker": 1
     },
     "SimpleRegression": {
         "model_size": 1,
@@ -334,9 +334,9 @@ _capability_matrix = {
         "misfit": 1,
         "log_likelihood": 0,
         "log_prior": 0,
-        "set_start_model": 1,
         "list_capabilities": 1,
         "set_obs_data": 1,
+        "set_start_model": 1,
         "set_start_mesh": 1
     }
 }
