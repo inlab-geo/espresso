d6dacee7841c28a2755516fb42e504c58de4e811
auggiemarignier <42379892+auggiemarignier@users.noreply.github.com>
2024-12-09 22:35:01 +0000

    Update esp_build to output generated at 3eb1795

--- src/espresso/capabilities.py
+++ src/espresso/capabilities.py
@@ -166,9 +166,9 @@ _capability_matrix = {
         "misfit": 0,
         "log_likelihood": 0,
         "log_prior": 0,
+        "list_capabilities": 1,
         "call_wavefront_tracker": 1,
-        "tmp_files": 1,
-        "list_capabilities": 1
+        "tmp_files": 1
     },
     "SimpleRegression": {
         "model_size": 1,
@@ -294,8 +294,8 @@ _capability_matrix = {
         "misfit": 0,
         "log_likelihood": 0,
         "log_prior": 0,
-        "rf": 1,
-        "list_capabilities": 1
+        "list_capabilities": 1,
+        "rf": 1
     },
     "SurfaceWaveTomography": {
         "model_size": 1,
@@ -313,8 +313,8 @@ _capability_matrix = {
         "misfit": 1,
         "log_likelihood": 0,
         "log_prior": 0,
-        "parameterization": 1,
         "list_capabilities": 1,
+        "parameterization": 1,
         "example_dict": 1
     },
     "Magnetotelluric1D": {
@@ -333,9 +333,9 @@ _capability_matrix = {
         "misfit": 1,
         "log_likelihood": 0,
         "log_prior": 0,
-        "set_start_model": 1,
+        "list_capabilities": 1,
         "set_obs_data": 1,
         "set_start_mesh": 1,
-        "list_capabilities": 1
+        "set_start_model": 1
     }
 }
