aee6c4be0afc1a851dab19576132130f32076f8e
auggiemarignier <42379892+auggiemarignier@users.noreply.github.com>
2024-12-12 05:06:49 +0000

    Update esp_build to output generated at 2e26d82

--- src/espresso/capabilities.py
+++ src/espresso/capabilities.py
@@ -166,9 +166,9 @@ _capability_matrix = {
         "misfit": 0,
         "log_likelihood": 0,
         "log_prior": 0,
-        "call_wavefront_tracker": 1,
         "tmp_files": 1,
-        "list_capabilities": 1
+        "list_capabilities": 1,
+        "call_wavefront_tracker": 1
     },
     "SimpleRegression": {
         "model_size": 1,
@@ -312,9 +312,9 @@ _capability_matrix = {
         "misfit": 1,
         "log_likelihood": 0,
         "log_prior": 0,
+        "parameterization": 1,
         "example_dict": 1,
-        "list_capabilities": 1,
-        "parameterization": 1
+        "list_capabilities": 1
     },
     "Magnetotelluric1D": {
         "model_size": 1,
@@ -333,8 +333,8 @@ _capability_matrix = {
         "log_likelihood": 0,
         "log_prior": 0,
         "set_obs_data": 1,
-        "set_start_model": 1,
         "set_start_mesh": 1,
-        "list_capabilities": 1
+        "list_capabilities": 1,
+        "set_start_model": 1
     }
 }
